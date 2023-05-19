70e0c9f1e69dffc8941aaf3d880a5419ebe64569
RealCryZed <bujhmgtnhjd@bk.ru>
2023-05-19 23:24:10 +0600

    Added one more test in FileHandlerTest

--- src/test/java/accounting/FileHandlerTest.java
+++ src/test/java/accounting/FileHandlerTest.java
@@ -10,4 +10,9 @@ public class FileHandlerTest {
     public void getAnimalsFromFile() {
         assertTrue(FileHandler.getAnimalsFromFile("dataset.txt").size() > 0);
     }
+
+    @Test
+    public void getInstructionsFromFile() {
+        assertTrue(FileHandler.getInstructionsFromFile("instructions.txt").size() > 0);
+    }
 }
