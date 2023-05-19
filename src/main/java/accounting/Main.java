9953871938d8739414248a0d37cc6f44db77f626
RealCryZed <bujhmgtnhjd@bk.ru>
2023-05-19 22:35:53 +0600

    Removed logger from Main class

--- src/main/java/accounting/Main.java
+++ src/main/java/accounting/Main.java
@@ -1,11 +1,8 @@
 package accounting;
 
 import java.util.List;
-import java.util.logging.Logger;
 
 public class Main {
-
-    private static final Logger log = Logger.getLogger(String.valueOf(Main.class));
 
     public static void main(String[] args) {
         List<Animal> animals = FileHandler.getAnimalsFromFile("dataset.txt");
